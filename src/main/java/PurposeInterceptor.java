372600a6864a6162f99c2c589c886c7e0c65231f
mkufel <m.kufel@hotmail.com>
2019-05-30 16:15:16 +0200

    added fake customer data, moved the customer-topic-policy mapping to the PurposeInterceptor

--- src/main/java/PurposeInterceptor.java
+++ src/main/java/PurposeInterceptor.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import io.moquette.interception.AbstractInterceptHandler;
 import io.moquette.interception.messages.InterceptAcknowledgedMessage;
 import io.moquette.interception.messages.InterceptConnectMessage;
@@ -5,14 +6,18 @@ import io.moquette.interception.messages.InterceptPublishMessage;
 import io.moquette.interception.messages.InterceptSubscribeMessage;
 import yappl.YaPPL;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class PurposeInterceptor extends AbstractInterceptHandler {
 
-    BrokerClient brokerClient = new BrokerClient();
+    ArrayList<Customer> customers = new ArrayList<>();
+    Map<String, Map<String, String>> customerIdToTopicPolicyMapping = new HashMap<>();
     YaPPL policyHandler = new YaPPL();
+    Faker faker = new Faker();
 
     @Override
     public void onConnect(InterceptConnectMessage msg) {
@@ -29,7 +34,7 @@ public class PurposeInterceptor extends AbstractInterceptHandler {
         final String decodedPayload = convertPayloadToBytes(msg);
         System.out.println("Received on topic: " + msg.getTopicName() + " content: " + decodedPayload);
 
-        String policyId = brokerClient.getTopicPolicyIdForCustomer(msg.getClientID(), msg.getTopicName());
+        String policyId = customerIdToTopicPolicyMapping.get(msg.getClientID()).get(msg.getTopicName());
         if (policyId != null) {
             // policyHandler.getPolicy(policyId)
             // For each purpose
@@ -56,6 +61,20 @@ public class PurposeInterceptor extends AbstractInterceptHandler {
 
         String customerId = msg.getClientID();
         String topic = msg.getTopicFilter();
-        brokerClient.updateTopicPolicy(customerId, topic, policyHandler.generateNewPolicyId());
+        customers.add(
+            new Customer(
+                customerId,
+                faker.funnyName().toString(),
+                faker.address().toString(),
+                faker.phoneNumber().toString()
+            )
+        );
+
+        this.updateTopicPolicyMapping(customerId, topic, policyHandler.generateNewPolicyId());
+    }
+
+    private void updateTopicPolicyMapping(String customerId, String topic, String policyId) {
+        Map<String, String> tempMap = new HashMap<String, String>() {{ put(topic, policyId); }};
+        customerIdToTopicPolicyMapping.put(customerId, tempMap);
     }
 }
