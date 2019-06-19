55c212d28bc8505674f4c6b858f75612cd46af9f
Gergely Bihary <biharygergo@gmail.com>
2019-06-19 12:04:41 +0200

    Interfaces for utilizers

--- src/main/java/BrokerClient.java
+++ src/main/java/BrokerClient.java
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.RandomStringUtils;
 import org.fusesource.mqtt.client.Callback;
 import org.fusesource.mqtt.client.CallbackConnection;
 import org.fusesource.mqtt.client.MQTT;
@@ -14,11 +15,15 @@ public class BrokerClient {
             // mqtt.setHost("localhost", 1883);
             mqtt.setHost(host, port);
             mqtt.setHost("tcp://" + host + ":" + port);
+            //System.out.println("GIVEN_ID_" + Math.random());
+            //mqtt.setClientId("GIVEN_ID_" + Math.random());
+            //System.out.println(mqtt.getClientId());
+            mqtt.setClientId(RandomStringUtils.randomAlphabetic(23));
+
             connection = mqtt.callbackConnection();
             connection.connect(new Callback<Void>() {
                 @Override
                 public void onSuccess(Void value) {
-
                 }
 
                 @Override
@@ -34,10 +39,10 @@ public class BrokerClient {
 
     public void sendMessage(String topic, String message) {
         try {
+
             connection.publish(topic, message.getBytes(), QoS.AT_LEAST_ONCE, false, new Callback<Void>() {
                 @Override
                 public void onSuccess(Void value) {
-
                 }
 
                 @Override
---
+++ src/main/java/UtilizerParser.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilizerParser {
+
+    public List<Utilizer> getUtilizersFromConfig() {
+        return new ArrayList<>();
+    }
+
+    public List<String> getAvailableUtilizerIds() {
+        return new ArrayList<>();
+    }
+}
+
