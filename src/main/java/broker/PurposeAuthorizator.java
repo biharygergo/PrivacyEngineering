35c3aa9f74b2bc9d6855be7dd343d7708242332a
Gergely Bihary <biharygergo@gmail.com>
2019-06-19 15:07:36 +0200

    Authorizator extension

--- src/main/java/broker/PurposeAuthorizator.java
+++ src/main/java/broker/PurposeAuthorizator.java
@@ -7,18 +7,20 @@ public class PurposeAuthorizator implements IAuthorizator {
 
     @Override
     public boolean canWrite(Topic topic, String user, String client) {
-        return !topic.toString().contains("purpose") || client.contains("4c697849-34d8-4159-879f-");
+        return true;
+        //return !topic.toString().contains("purpose") || client.contains("4c697849-34d8-4159-879f-");
     }
 
     @Override
     public boolean canRead(Topic topic, String user, String client) {
-        if (client.equals("benchmarker") || client.contains("4c697849-34d8-4159-879f-")) {
+        return true;
+   /*     if (client.equals("benchmarker") || client.contains("4c697849-34d8-4159-879f-")) {
             return true;
         }
         if (topic.toString().contains("purpose") &&
                 client.equals(topic.toString().split("/")[1])) {
             return true;
         }
-        return false;
+        return false;*/
     }
 }
