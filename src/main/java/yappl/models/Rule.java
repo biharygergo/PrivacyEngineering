a3da53dc1f4167fa869eedd55e50a70ffe0c12a0
Gergely Bihary <biharygergo@gmail.com>
2019-06-30 14:38:04 +0200

    Finished YaPPL code documentation

--- src/main/java/yappl/models/Rule.java
+++ src/main/java/yappl/models/Rule.java
@@ -17,50 +17,86 @@ public class Rule {
     @JsonProperty("exp_date")
     private String expDate = "0000-00-00T00:00:00.00Z";
 
+    /**
+     * @return The purposes that are available in the rule.
+     */
     public Purpose getPurpose() {
         return purpose;
     }
 
+    /**
+     * @param purpose The purpose that should be set.
+     */
     public void setPurpose(Purpose purpose) {
         this.purpose = purpose;
     }
 
+    /**
+     * @return The utilizers in the rule.
+     */
     public Utilizer getUtilizer() {
         return utilizer;
     }
 
+    /**
+     * @param utilizer The Utilizer that should be set.
+     */
     public void setUtilizer(Utilizer utilizer) {
         this.utilizer = utilizer;
     }
 
+    /**
+     * @return A list of transformations in the rule.
+     */
     public List<Transformation> getTransformation() {
         return transformation;
     }
 
+    /**
+     * @param transformation The list of transformations that should be set.
+     */
     public void setTransformation(List<Transformation> transformation) {
         this.transformation = transformation;
     }
 
+    /**
+     * @return The start of the validity of the rule in a String timestamp.
+     */
     public String getValidFrom() {
         return validFrom;
     }
 
+    /**
+     * @param validFrom The valid from time as a String timestamp.
+     */
     public void setValidFrom(String validFrom) {
         this.validFrom = validFrom;
     }
 
+    /**
+     * @return The expiry date as a string timestamp.
+     */
     public String getExpDate() {
         return expDate;
     }
 
+    /**
+     * @param expDate The expiry date that should be set as a string timestamp.
+     */
     public void setExpDate(String expDate) {
         this.expDate = expDate;
     }
 
+    /**
+     * @return The id of the rule.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id The id that should be set for the Rule.
+     */
     public void setId(int id) {
         this.id = id;
     }
--- src/main/java/yappl/models/Transformation.java
+++ src/main/java/yappl/models/Transformation.java
@@ -7,18 +7,30 @@ public class Transformation {
     @JsonProperty("tr_func")
     private String truncateFunction;
 
+    /**
+     * @return The attribute of the transformation.
+     */
     public String getAttribute() {
         return attribute;
     }
 
+    /**
+     * @param attribute The attribute that should be set for this transformation.
+     */
     public void setAttribute(String attribute) {
         this.attribute = attribute;
     }
 
+    /**
+     * @return The truncate function of the transformation.
+     */
     public String getTruncateFunction() {
         return truncateFunction;
     }
 
+    /**
+     * @param truncateFunction The function that should be set for the transformation.
+     */
     public void setTruncateFunction(String truncateFunction) {
         this.truncateFunction = truncateFunction;
     }
