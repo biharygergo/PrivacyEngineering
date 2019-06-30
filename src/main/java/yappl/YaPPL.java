7f1fc948f4ebfb1305a17e08538f6d71ef55bc21
Gergely Bihary <biharygergo@gmail.com>
2019-06-30 14:12:04 +0200

    Yappl comments

--- src/main/java/yappl/YaPPL.java
+++ src/main/java/yappl/YaPPL.java
@@ -12,9 +12,18 @@ import yappl.models.Preference;
 
 import java.io.IOException;
 
+/**
+ * Helper class to parse and validate YaPPL policies.
+ */
 public class YaPPL {
-    static ObjectMapper mapper = new ObjectMapper(); // create once, reuse
+    static ObjectMapper mapper = new ObjectMapper();
 
+    /**
+     * Validates a JSON policy.
+     * @param policy The JSON string to validate
+     * @throws IOException Thrown when the schema resource file is not found.
+     * @throws YaPPLFormatException Thrown when there have been errors validating the schema.
+     */
     public static void validate(String policy) throws IOException, YaPPLFormatException {
         JsonNode schemaNode = loadResource("/YaPPL_schema.json");
         JsonNode toValidate = mapper.readTree(policy);
@@ -34,6 +43,12 @@ public class YaPPL {
         }
     }
 
+    /**
+     * Parses a JSON policy string into a Policy object.
+     * @param jsonPolicy The JSON policy string.
+     * @return The parsed Policy object
+     * @throws IOException Thrown if the policy could not be read.
+     */
     public static Policy parse(String jsonPolicy) throws IOException {
 
         Policy parsedPolicy = mapper.readValue(jsonPolicy, Policy.class);
@@ -44,6 +59,12 @@ public class YaPPL {
         return parsedPolicy;
     }
 
+    /**
+     * Loads a JSON resource by its name.
+     * @param name The name of the resource file.
+     * @return Returns a usable JsonNode object.
+     * @throws IOException Thrown if the file could not be read.
+     */
     public static JsonNode loadResource(final String name)
             throws IOException {
         final String packageName = YaPPL.class.getPackage().getName();
